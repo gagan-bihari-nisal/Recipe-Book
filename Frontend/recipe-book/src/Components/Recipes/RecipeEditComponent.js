995ef957a4d3e84f62662771e0c4344fccd19d7b
gagan-bihari-nisal <gagannisal@gmail.com>
2024-03-14 20:17:41 +0530

    implement add/remove ingredient and step

--- Frontend/recipe-book/src/Components/Recipes/RecipeEditComponent.js
+++ Frontend/recipe-book/src/Components/Recipes/RecipeEditComponent.js
@@ -1,31 +1,79 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchRecipesSuccess } from '../../Store/Recipes/RecipeActions'
+import '../../Styles/RecipeEditComponent.css'
 class RecipeEditComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
       id: this.props.params.id,
-      editMode: this.props.params.id === null ? false : true
+      editMode: this.props.params.id === null ? false : true,
+      name: '',
+      description: '',
+      imageFile: '',
+      ingredients: [],
+      steps: [],
     }
   }
 
   componentDidMount() {
-
+    if (this.state.editMode) {
+      const { recipes } = this.props.recipes;
+      const recipe = recipes.filter(recipe => recipe.id === parseInt(this.state.id))[0];
+      this.setState({
+        name: recipe.name,
+        description: recipe.description,
+        ingredients: recipe.ingredients,
+        steps: recipe.steps,
+        imageFile: recipe.imageFile,
+      })
+    }
   }
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+  handleChange = (event, index, type) => {
+    const { name, value } = event.target;
+    if (type === 'ingredient') {
+      const updatedIngredients = [...this.state.ingredients];
+      updatedIngredients[index] = { ...updatedIngredients[index], [name]: value };
+      this.setState({ ingredients: updatedIngredients });
+    } else if (type === 'step') {
+      const updatedSteps = [...this.state.steps];
+      updatedSteps[index] = { ...updatedSteps[index], [name]: value };
+      this.setState({ steps: updatedSteps });
+    } else {
+      this.setState({ [name]: value });
+    }
   }
+
 
   handleSubmit = (event) => {
+    event.preventDefault();
+  }
 
+  addIngredient = (event) => {
+    const updatedIngredients = [...this.state.ingredients];
+    updatedIngredients.push({});
+    this.setState({ ingredients: updatedIngredients });
+  }
+
+  addStep = (event) => {
+    const updatedSteps = [...this.state.steps];
+    updatedSteps.push({});
+    this.setState({ steps: updatedSteps });
+  }
+
+  deleteIngredient = (index) => {
+    const updatedIngredients = [...this.state.ingredients];
+    updatedIngredients.splice(index, 1);
+    this.setState({ ingredients: updatedIngredients });
+  }
+
+  deleteStep = (index) => { 
+    const updatedSteps = [...this.state.steps];
+    updatedSteps.splice(index, 1);
+    this.setState({ steps: updatedSteps });
   }
   render() {
-    const { recipes } = this.props.recipes;
-    const recipe = recipes.filter(recipe => recipe.id === parseInt(this.state.id))[0];
+    const { name, description, ingredients, steps, imageFile } = this.state;
     return (
       <div className="RecipeEditComponent">
         <div className="row">
@@ -45,7 +93,8 @@ class RecipeEditComponent extends Component {
                 <div className="form-floating">
                   <input type="text" className="form-control" id="name" name="name"
                     onChange={this.handleChange}
-                    placeholder='Recipe Name' />
+                    placeholder='Recipe Name'
+                    value={name} />
                   <label htmlFor="name">Recipe Name</label>
                 </div>
               </div>
@@ -57,14 +106,15 @@ class RecipeEditComponent extends Component {
                 <div className="form-floating">
                   <input type="text" className="form-control" id="description" name="description"
                     onChange={this.handleChange}
+                    value={description}
                     placeholder='Recipe Description' />
                   <label htmlFor="description">Recipe Description</label>
                 </div>
               </div>
 
 
-              <div class="input-group mb-3">
-                <label class="input-group-text" for="imageFile">Recipe Image</label>
+              <div className="input-group mb-3">
+                <label className="input-group-text" for="imageFile">Recipe Image</label>
                 <input type="file" className="form-control" style={{ fontSize: '17px' }} id="imageFile" name="imageFile"
                   onChange={this.handleChange}
                   placeholder='Recipe Image' />
@@ -73,47 +123,49 @@ class RecipeEditComponent extends Component {
               <hr className='text-white' />
               <div className="row">
                 <div className="col-xs-12">
-                  {recipe.ingredients.map(ingredient => {
+                  {ingredients.map((ingredient, index) => {
                     return <div className="input-group my-3">
                       <span className="input-group-text">
                         <i className="bi bi-cake text-light" style={{ fontSize: "20px" }}></i>
                       </span>
                       <div className="form-floating">
-                        <input type="text" className="form-control" id="ingredients" name="ingredients"
-                          onChange={this.handleChange}
+                        <input type="text" className="form-control" id={`ingredient-${index}`} name="ingredientName"
+                          onChange={(e) => this.handleChange(e, index, 'ingredient')}
                           value={ingredient.ingredientName}
                           placeholder='Recipe Ingredient' />
+
                         <label htmlFor="ingredients">Recipe Ingredient</label>
 
                       </div>
-                      <button class="btn btn-danger" type="button" id="button-addon2">X</button>
+                      <button className="btn btn-danger" onClick={() => this.deleteIngredient(index)} type="button" id="button-addon2">X</button>
                     </div>
                   })}
                 </div>
               </div>
-              <button class="btn btn-success" type="button" id="button-addon2">Add Ingredient</button>
+              <button className="btn btn-success" type="button" id="button-addon2" onClick={this.addIngredient}>Add Ingredient</button>
               <hr className='text-white' />
 
               <div className="row">
                 <div className="col-xs-12">
-                  {recipe.steps.map(step => {
+                  {steps.map((step,index) => {
                     return <div className="input-group my-3">
                       <span className="input-group-text">
                         <i className="bi bi-cake text-light" style={{ fontSize: "20px" }}></i>
                       </span>
                       <div className="form-floating">
-                        <input type="text" className="form-control" id="steps" name="steps"
-                          onChange={this.handleChange}
+                        <input type="text" className="form-control" id={`step-${index}`} name="step"
+                          onChange={(e) => this.handleChange(e, index, 'step')}
                           value={step.step}
-                          placeholder='Recipe Ingredient' />
+                          placeholder='Recipe Procedure' />
+
                         <label htmlFor="ingredients">Recipe Procedure</label>
                       </div>
-                      <button class="btn btn-danger" type="button" id="button-addon2">X</button>
+                      <button className="btn btn-danger" type="button" onClick={() => this.deleteStep(index)}id="button-addon2">X</button>
                     </div>
                   })}
                 </div>
               </div>
-              <button class="btn btn-success" type="button" id="button-addon2">Add Next Step</button>
+              <button className="btn btn-success" type="button"  onClick={this.addStep}id="button-addon2">Add Next Step</button>
               <hr className='text-white' />
 
             </form>
@@ -128,8 +180,4 @@ const mapStateToProps = (state) => ({
   recipes: state.recipes
 });
 
-const mapDispatchToProps = {
-  fetchRecipesSuccess
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(RecipeEditComponent);
+export default connect(mapStateToProps)(RecipeEditComponent);
