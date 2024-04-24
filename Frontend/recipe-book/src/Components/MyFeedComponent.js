3163662d9431c22e74f42c880bd8fd2653231c4c
gagan-bihari-nisal <gagannisal@gmail.com>
2024-04-24 17:59:49 +0530

    update feed ui

--- Frontend/recipe-book/src/Components/MyFeedComponent.js
+++ Frontend/recipe-book/src/Components/MyFeedComponent.js
@@ -7,7 +7,6 @@ export default class MyFeedComponent extends Component {
         super(props);
         this.state = {
             recipes: [],
-            showDetails: -1, // Add this line
             searchQuery: ''
         };
     }
@@ -15,15 +14,11 @@ export default class MyFeedComponent extends Component {
     handleSearchChange = (event) => {
         this.setState({ searchQuery: event.target.value });
     }
-    handleExpandClick = (index) => {
-        this.setState(prevState => ({ showDetails: prevState.showDetails === index ? -1 : index }));
-    };
 
     componentDidMount() {
         const recipeService = new RecipeService(store);
         recipeService.getOtherRecipes()
             .then((response) => {
-                // response data contains recipe image and it needs to be modified to display the image
                 const recipes = response.data.map((recipe) => {
                     return recipe;
                 });
@@ -40,20 +35,27 @@ export default class MyFeedComponent extends Component {
         );
         return (
             <>
-                <div className="container-fluid my-3" style={{ width: "500px" }}>
-
-                    <div className="form-floating">
-                        <input type="text" className="form-control" id="searchQuery" name="searchQuery"
-                            value={this.state.searchQuery}
-                            placeholder='Search Recipes'
-                            onChange={this.handleSearchChange}
-                        />
-                        <label htmlFor="searchQuery">Search Recipes</label>
+                <div className="container my-3" style={{ maxWidth: "70%" }}>
+                    <div className="row">
+                        <div className="col-12 mx-auto">
+                            <div className="form-floating">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="searchQuery"
+                                    name="searchQuery"
+                                    value={this.state.searchQuery}
+                                    placeholder='Search Recipes'
+                                    onChange={this.handleSearchChange}
+                                />
+                                <label htmlFor="searchQuery">Search Recipes</label>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {
                     filteredRecipes.length === 0 &&
-                    <div className="container-fluid my-3 d-flex flex-column justify-content-center align-items-center" style={{ width: "500px", height: "50vh" }}>
+                    <div className="container my-3 d-flex flex-column justify-content-center align-items-center" style={{ height: "50vh" }}>
                         <i className="bi bi-emoji-frown text-light mb-3" style={{ fontSize: "5rem" }}></i>
                         <h2 className="fs-5 text-uppercase fw-bolder text-light">
                             No recipes found
@@ -62,70 +64,68 @@ export default class MyFeedComponent extends Component {
                 }
                 {filteredRecipes.map((recipe, index) => {
                     return (
-                        <div className="container-fluid my-3 bg-light" style={{ width: "500px" }}>
+                        <div className="container my-3 bg-dark text-light">
                             <div className="row">
-                                <div className="col text-white">
-                                    <img src={recipe.image} alt="" style={{ height: '200px', width: '200px', padding: "10px" }} />
+                                <div className="col-12 col-md-6">
+                                    <img src={recipe.image} alt={recipe.name} className="img-fluid rounded" style={{ padding: "10px" }} />
                                 </div>
-                                <div className="col p-2">
-                                    <h2 className='text-uppercase fw-bolder text-dark'>{recipe.name}</h2>
-                                    <h2 className="fs-5  fw-lighter text-dark">
+                                <div className="col-12 col-md-6 px-3">
+                                    <h2 className='text-uppercase fw-bolder text-light'>{recipe.name}</h2>
+                                    <h2 className="fs-5 fw-lighter text-light">
                                         <i>{recipe.description}</i>
                                     </h2>
                                 </div>
-
                             </div>
-                            <p ><small class="text-muted">Uploaded By: @{recipe.username}</small></p>
-                            <div className="row">
-                                <div className="col  d-flex justify-content-center align-items-center hover-effect
-hover-effect" style={{
-                                        cursor: "pointer",
-                                        bottom: "0%", right: "50%",
-                                        backgroundColor: "darkcyan"
-
-                                    }} onClick={() => this.handleExpandClick(index)}>
-                                    {this.state.showDetails === index
-                                        ? <i className='bi bi-chevron-up fs-5 text-white '></i>
-                                        : <i className='bi bi-chevron-down fs-5 text-white '></i>
-                                    }
+                            <p><small class="px-2 text-secondary">Uploaded By: @{recipe.username}</small></p>
+                            <div className="accordion accordion-flush rounded pb-2 px-2" id="accordionParent">
+                                <div className="accordion-item">
+                                    <h3 className="accordion-header" id={`heading${index}`}>
+                                        <button className="accordion-button collapsed bg-secondary text-light" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="false" aria-controls={`collapse${index}`}>
+                                            More Details
+                                        </button>
+                                    </h3>
+                                    <div id={`collapse${index}`} className="accordion-collapse collapse" aria-labelledby={`heading${index}`} data-bs-parent="#accordionParent">
+                                        <div className="accordion-body bg-dark text-light">
+                                            {recipe.ingredients.length > 0 && <h4 className="fs-5 text-uppercase fw-bolder text-light">
+                                                Ingredients
+                                            </h4>}
+                                            <ul className="list-group">
+                                                {recipe.ingredients.length > 0 &&
+                                                    recipe.ingredients.map((ingredient, index) => (
+                                                        <li key={index} className="list-group-item bg-dark text-light">
+                                                            <div className="row">
+                                                                <div className="col-2">
+                                                                    <span className="badge rounded-pill bg-light text-dark">{index + 1}</span>
+                                                                </div>
+                                                                <div className="col-10">
+                                                                    {ingredient.ingredientName}
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                            </ul>
+                                            {recipe.steps.length > 0 && <h4 className="fs-5 text-uppercase fw-bolder my-2 text-light">
+                                                Procedure
+                                            </h4>}
+                                            <ul className="list-group">
+                                                {recipe.steps.length > 0 &&
+                                                    recipe.steps.map((procedure, index) => (
+                                                        <li key={index} className="list-group-item bg-dark text-light">
+                                                            <div className="row">
+                                                                <div className="col-3">
+                                                                    <span className="badge rounded-pill bg-light text-dark">Step {index + 1}</span>
+                                                                </div>
+                                                                <div className="col-9">
+                                                                    {procedure.step}
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div className="row">
-                                {this.state.showDetails === index && (
-                                    <div className="card text-dark bg-light">
-                                        {recipe.ingredients.length > 0 && <h4 className="fs-5 text-uppercase fw-bolder text-dark">
-                                            Ingredients
-                                        </h4>}
-                                        <ul className="list-group">
-                                            {recipe.ingredients.length > 0 &&
-                                                recipe.ingredients.map((ingredient, index) => (
-                                                    <li key={index} className="list-group-item ">
-                                                        <div>
-                                                            <span className="badge rounded-pill bg-dark">{index + 1}</span>
-                                                            &nbsp;&nbsp;&nbsp;{ingredient.ingredientName}
-                                                        </div>
-                                                    </li>
-                                                ))}
-                                        </ul>
-                                        {recipe.steps.length > 0 && <h4 className="fs-5 text-uppercase fw-bolder text-dark">
-                                            Procedure
-                                        </h4>}
-                                        <ul className="list-group">
-                                            {recipe.steps.length > 0 &&
-                                                recipe.steps.map((procedure, index) => (
-                                                    <li key={index} className="list-group-item ">
-                                                        <div>
-                                                            <span className="badge rounded-pill bg-dark ">Step {index + 1}</span>
-                                                            &nbsp;&nbsp;&nbsp;{procedure.step}
-                                                        </div>
-                                                    </li>
-                                                ))}
-                                        </ul>
-                                    </div>
-                                )}
-                            </div>
-
                         </div>
                     )
                 })}
